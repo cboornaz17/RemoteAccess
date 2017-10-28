7e6b78ce6f4caaaf15e1ccff89c684094e7a4e12
jbeck18 <j.beck18@me.com>
2017-10-28 12:05:54 -0400

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,84 @@
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+
+	public final static int SOCKET_PORT = 1234;
+	public final static String SERVER = "127.0.0.1";
+	public final static int FILE_SIZE = 50000000;
+
+	public static void main(String[] args) throws IOException {		
+		while(true) {
+			
+			Scanner scanner = new Scanner(System.in);
+			Socket socket = new Socket("128.113.153.85", 1234);
+			Scanner serverScanner = new Scanner(socket.getInputStream());
+			
+			System.out.println("Enter a command");
+			String cmd = scanner.nextLine();
+			
+			if(cmd.equalsIgnoreCase("exit")) {
+				System.out.println("Exiting...");
+				break;
+			}
+			
+			PrintStream p = new PrintStream(socket.getOutputStream());
+			p.println(cmd);
+			
+			String[] arguments = cmd.split(" ");
+			
+			if(arguments[0].equalsIgnoreCase("get")) {
+				if(arguments.length == 2) {
+					getFile("/Users/josh/Desktop/Temp/Client/" + arguments[1], socket);
+					System.out.print("\n\n");
+				} else if(arguments.length == 3) {
+					getFile("/Users/josh/Desktop/Temp/Client/" + arguments[2], socket);
+					System.out.println("\n\n");
+				}
+				continue;
+			}
+			
+			String result = "";
+			while(serverScanner.hasNextLine()) {
+				result += serverScanner.nextLine() + "\n";
+			}
+			
+			System.out.println(result);
+		}
+	}
+
+	public static void getFile(String FILE_TO_RECEIVED, Socket sock) throws UnknownHostException, IOException {
+		int bytesRead;
+		int current = 0;
+		FileOutputStream fos = null;
+		BufferedOutputStream bos = null;
+
+		try {
+			byte[] mybytearray = new byte[FILE_SIZE];
+			InputStream is = sock.getInputStream();
+			fos = new FileOutputStream(FILE_TO_RECEIVED);
+			bos = new BufferedOutputStream(fos);
+
+			do {
+				bytesRead = is.read(mybytearray, current, (mybytearray.length - current));
+				if (bytesRead >= 0)
+					current += bytesRead;
+			} while (bytesRead < 0);
+
+			bos.write(mybytearray, 0, current);
+			bos.flush();
+			System.out.println("File " + FILE_TO_RECEIVED + " downloaded (" + current + " bytes read)");
+		} finally {
+			if (fos != null)
+				fos.close();
+			if (bos != null)
+				bos.close();
+		}
+	}
+}
---
+++ Server.java
@@ -0,0 +1,171 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+	
+	public static final int SOCKET_PORT = 1234;
+	private static File currentFile = new File("/Users/josh");
+	
+	public static void main(String[] args) throws IOException {
+		ServerSocket socket = new ServerSocket(1234);
+		
+		try {
+			while(true) {
+				Socket client = socket.accept();
+				try {
+					Scanner clientScanner = new Scanner(client.getInputStream());		
+					
+					String input = "";
+					try {
+						input = clientScanner.nextLine();
+					} catch(Exception e) {
+						
+					}
+					
+					String[] arguments = input.split(" ");
+					
+					if(arguments.length == 1) {
+						if(arguments[0].equalsIgnoreCase("exit")) {
+							continue;
+						} else if(arguments[0].equalsIgnoreCase("clear")) {
+							PrintStream p = new PrintStream(client.getOutputStream());
+							p.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+							continue;
+						}
+					}
+					
+					if(arguments.length >= 1) {
+						if(arguments[0].equalsIgnoreCase("ls")) {
+							PrintStream p = new PrintStream(client.getOutputStream());
+							String dir = "";
+							dir = printDirectory(currentFile);
+							if(arguments.length >= 2) {
+								if(arguments[1].equalsIgnoreCase("-a")) {
+									dir = printDirectoryAll(currentFile);
+								}
+							}
+							
+							p.println(dir);
+						}
+						
+						if(arguments.length >= 2) {
+							if(arguments[0].equalsIgnoreCase("cd")) {
+								String fileName = arguments[1];
+								PrintStream p = new PrintStream(client.getOutputStream());
+								if(fileName.equalsIgnoreCase("../")) {
+									File newFile = currentFile.getParentFile();
+									if(!newFile.exists()) {
+										p.println(printDirectory(currentFile));
+									} else {
+										currentFile = newFile;
+										p.println(currentFile);
+									}
+								} else {
+									String newFile = currentFile.getPath() + "/" + fileName;
+									File f = new File(newFile);
+									
+									if(f.exists()) {
+										currentFile = new File(f.getAbsolutePath());
+										p.println(printDirectory(currentFile));
+									}
+								}
+							} else if(arguments[0].equalsIgnoreCase("get")) {
+								if(arguments.length >= 2) {
+									String fileName = arguments[1];									
+									sendFile(currentFile.getAbsolutePath() + "/" + fileName, client);
+								}
+							}
+						}
+					}
+				} finally {
+						client.close();
+				}
+			} 
+		} finally {
+			socket.close();
+			currentFile = new File("Users/josh/Desktop");
+		}		
+	}
+	
+	private static String printDirectory(File file) {
+		String s = "";
+		
+		File[] files = file.listFiles();
+		
+		if(files == null)
+			return s;
+		
+		int count = 0;
+		for(int i = 0; i < files.length; i++) {		
+			if(count%4 == 0 && count != 0)
+				s += "\n";
+			
+			if(files[i].isDirectory()) {
+				if(!files[i].getName().startsWith(".")) {
+					s += files[i].getName() + "/\t\t";
+					count++;
+				}
+			} else {
+				if(!files[i].getName().startsWith(".")) {
+					s += files[i].getName() + "\t\t";
+					count++;
+				}
+			}
+		}
+		s += "\n\n";
+		return s;
+	}
+	
+	private static String printDirectoryAll(File file) {
+		String s = "";
+		
+		File[] files = file.listFiles();
+		
+		for(int i = 0; i < files.length; i++) {
+			
+			if(i%4 == 0)
+				s += "\n";
+			
+			if(files[i].isDirectory())
+				s += files[i].getName() + "/\t\t";
+			else {
+				s += files[i].getName() + "\t\t";
+			}
+		}
+		s += "\n\n";
+		return s;
+	}
+	
+	public static void sendFile(String FILE_TO_SEND, Socket sock) throws IOException {
+		FileInputStream fis = null;
+		BufferedInputStream bis = null;
+		OutputStream os = null;
+
+		try{
+			File myFile = new File(FILE_TO_SEND);
+			byte[] mybytearray = new byte[(int) myFile.length()];
+			fis = new FileInputStream(myFile);
+			bis = new BufferedInputStream(fis);
+			bis.read(mybytearray, 0, mybytearray.length);
+			os = sock.getOutputStream();
+			System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
+			os.write(mybytearray, 0, mybytearray.length);
+			os.flush();
+			System.out.println("Done.");
+		} finally {
+			if (bis != null)
+				bis.close();
+			if (os != null)
+				os.close();
+			if (sock != null)
+				sock.close();
+		}
+	}
+}
